fcf00d38e5d54a06d9901da05c59982b9ef2e95a
Neeraj Kumar <38447673+neerajks77@users.noreply.github.com>
2021-10-02 10:13:17 +0530

    Add files via upload

---
+++ CreateServicePrincipal.ps1
@@ -0,0 +1,12 @@
+﻿#This function creates an Azure AD Service Principal
+function CreateServicePrincipal {
+  param($username, $password)
+
+	$credproperties = @{
+		StartDate = Get-Date
+		EndDate = Get-Date -Year 2024
+		Password = $password
+  }
+  $cred= New-Object Microsoft.Azure.Commands.ActiveDirectory.PSADPasswordCredential -Property $credproperties
+  $sp = New-AzAdServicePrincipal -DisplayName $username -PasswordCredential $cred
+}
