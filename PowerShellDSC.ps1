69ecae32dec8189236d174e09992be63e5f2bf73
Neeraj Kumar <38447673+neerajks77@users.noreply.github.com>
2022-05-07 09:08:15 +0530

    Update PowerShellDSC.ps1

--- PowerShellDSC.ps1
+++ PowerShellDSC.ps1
@@ -2,16 +2,18 @@
 Configuration WebServer
 {
     param(
-    [string] $NodeNames = "localhost",
-    [string] $AcctKey = "gE7MNh3+HPF1nJAhtRhzDSqJWgizpE/g572kZ8s4D0jAhh+ocpKhP4NGRURAyaqq5CUXhnru7FwngrFqRhD9tg=="
+	[parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string] $NodeNames,
+	[string] $AcctKey = "gE7MNh3+HPF1nJAhtRhzDSqJWgizpE/g572kZ8s4D0jAhh+ocpKhP4NGRURAyaqq5CUXhnru7FwngrFqRhD9tg=="
     )
 
-    Import-DscResource -ModuleName PsDesiredStateConfiguration
+    Import-DscResource -Name Service -Module PsDesiredStateConfiguration
 
-    [String] $StorageAccountName = "Azure\linkedinautomation"
-
+    [String] $StorageAccountName = "linkedinautomation"
+	[String] $StorageAccountUserName = "Azure\" + $StorageAccountName
     $secureacctKey = $AcctKey | ConvertTo-SecureString -AsPlainText -Force
-    $myStorageCredentials = New-Object System.Management.Automation.PSCredential ($StorageAccountName, $secureacctKey)
+    $myStorageCredentials = New-Object System.Management.Automation.PSCredential ($StorageAccountUserName, $secureacctKey)
     
     Node $NodeNames
     {
